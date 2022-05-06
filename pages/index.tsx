4bf006907e18ac24c759b202aab0757b68f2da45
oshanQQ <oshanqq@gmail.com>
2022-05-06 23:23:34 +0900

    💄 Use react gfm

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,26 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 const Home: NextPage = () => {
+  const markdown = `A paragraph with *emphasis* and **strong importance**.
+
+  > A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+  * Lists
+  * [ ] todo
+  * [x] done
+
+  A table:
+
+  | a | b |
+  | - | - |
+  `;
+
   return (
     <div className={styles.container}>
-      <ReactMarkdown># Hello, *world*!</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
     </div>
   );
 };
