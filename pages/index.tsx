0c70cce5aad1d06618b28468a714ad939197328d
oshanQQ <oshanqq@gmail.com>
2022-05-07 09:59:31 +0900

    ✨ Add syntax highlight

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 const Home: NextPage = () => {
   const markdown = `A paragraph with *emphasis* and **strong importance**.
@@ -18,11 +20,39 @@ const Home: NextPage = () => {
 
   | a | b |
   | - | - |
+
+  ~~~rust
+  let message = "world!"
+  println!("Hello, {}", message);
+  ~~~
   `;
 
   return (
     <div className={styles.container}>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                language={match[1]}
+                style={dracula as any}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {markdown}
+      </ReactMarkdown>
     </div>
   );
 };
