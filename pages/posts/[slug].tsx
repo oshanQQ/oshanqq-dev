235c19dc6fe0bc7f96ca60153f88ebe74b5044fd
oshanQQ <oshanqq@gmail.com>
2022-05-08 14:48:25 +0900

    🔥 Delete og image

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -33,7 +33,7 @@ const Post = ({ post, preview }: Props) => {
             <article className="mb-32">
               <Head>
                 <title>{post.title} | oshanqq dev</title>
-                <meta property="og:image" content={post.ogImage.url} />
+                <meta property="og:image" />
               </Head>
               <PostHeader title={post.title} date={post.date} />
               <PostBody content={post.content} />
--- types/post.ts
+++ types/post.ts
@@ -3,9 +3,6 @@ type PostType = {
   title: string;
   date: string;
   excerpt: string;
-  ogImage: {
-    url: string;
-  };
   content: string;
 };
 
