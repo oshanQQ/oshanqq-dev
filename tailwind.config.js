5469c77ae0f053f9a86f4d03554a427e7d6c3027
oshanQQ <oshanqq@gmail.com>
2022-05-08 17:15:26 +0900

    👷 Purge CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
+  mode: "jit",
   content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     fontFamily: {
